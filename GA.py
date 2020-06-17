2a75e60cdea26f9a7fb7f14db86a3b2b4aa37adc
nathan-faganello <faganello.nathan@gmail.com>
2020-06-17 17:02:36 +0200

    modif mutation()

--- GA.py
+++ GA.py
@@ -2,10 +2,20 @@ from random import Random
 
 generator=Random()
 
+def maximum(L):
+	maxi=L[0]
+	rang_max=0
+	for i in range(len(L)):
+		if L[i]>maxi:
+			rang_max=i
+	return 
+
 def mutation(C,pm,sigma):
 	x=generator.uniform(0,1)
 	if x<pm:
 		for D in C :
-			D[0]+=generator.gauss(0,sigma)
-			D[1]+=generator.gauss(0,sigma)
+			for y in D:
+				y+=generator.gauss(0,sigma)
 	return C
+
+
