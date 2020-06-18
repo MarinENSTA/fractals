01692200f9a896d975c53c2d33f2c176a0988446
MarinENSTA <62882158+MarinENSTA@users.noreply.github.com>
2020-06-18 21:01:53 +0200

    First GUI update
    Not perfect yet but... it's a first !

--- graphics.py
+++ graphics.py
@@ -3,8 +3,18 @@ if __name__ == '__main__':
     import matplotlib
     from matplotlib import colors
     import matplotlib.pyplot as plt
-
+    import pygame
+    from pygame.locals import *
     from julia import *
+    
+    prop = 1.5
+    pygame.init()
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((int(870*prop), int(400*prop)))
+    pygame.display.set_caption('JuliaSets')
+
+    fraclist = [0]*4
+    reclist = [0]*4
 
     xmin, xmax, xn = -1.25, +1.25, 3000/2
     ymin, ymax, yn = -1.25, +1.25, 2500/2
@@ -13,43 +23,83 @@ if __name__ == '__main__':
     horizon = 2.0 ** 40
     log_horizon = np.log(np.log(horizon))/np.log(2)
 
-    # C = [[0.285, 0.01]]*100
-
-    # C = [[0.285,0.013]]*100
-
-    # C = [[0,-1]]*100
-
-    # C = [[0.3,0.5]]*100
-
-    C = [[0.285, 0.01],[0.285,0.013],[0,-1],[0.3,0.5]]*25
-
-    Z, N = julia_set(xmin, xmax, ymin, ymax, xn, yn, C, maxiter, horizon)
-
-    # Normalized recount as explained in:
-    # https://linas.org/art-gallery/escape/smooth.html
-    # https://www.ibm.com/developerworks/community/blogs/jfp/entry/My_Christmas_Gift
-
-    #  This line will generate warnings for null values but it is faster to
-    # process them afterwards using the nan_to_num
-    with np.errstate(invalid='ignore'):
-        M = np.nan_to_num(N + 1 -
-                          np.log(np.log(abs(Z)))/np.log(2) +
-                          log_horizon)
-
     dpi = 72
     width = 10
     height = 10*yn/xn
-    fig = plt.figure(figsize=(width, height), dpi=dpi)
-    ax = fig.add_axes([0.0, 0.0, 1.0, 1.0], frameon=False, aspect=1)
 
-    # Shaded rendering
-    light = colors.LightSource(azdeg=130, altdeg=100)
-    M = light.shade(M, cmap=plt.cm.hot, vert_exag=1.5, norm=colors.PowerNorm(0.3), blend_mode='hsv')
-    # M = light.shade(M, cmap=plt.cm.coolwarm) # or cmap=plt.cm.coolwarm
-    # see https://matplotlib.org/examples/color/colormaps_reference.html
-    plt.imshow(M, extent=[xmin, xmax, ymin, ymax], interpolation="bicubic")
-    ax.set_xticks([])
-    ax.set_yticks([])
-# faire tourner source azdeg et altdef
-    plt.show()
+    C0 = [[-0.4,0.6]]*100
+    C1 = [[0.285, 0.01]]*100
+    C2 = [[0.3,0.5]]*100
+    C3 = [[0,-1]]*100
+    C = [C0, C1, C2, C3]
+    
+    quit = False
+    while not quit:
+        #clock.tick(30)
+        fig = plt.figure(figsize=(width, height), dpi=dpi)
+        ax = fig.add_axes([0.0, 0.0, 1.0, 1.0], frameon=False, aspect=1)
+        
+        # Shaded rendering
+        light = colors.LightSource(azdeg=315, altdeg=10)
+        screen.fill((0,0,0))
+        for i in range(4):
+            for event in pygame.event.get():
+                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                    pygame.quit()
+                elif event.type == KEYDOWN:
+                    pygame.quit()
+            
+            Z, N = julia_set(xmin, xmax, ymin, ymax, xn, yn, C[i], maxiter, horizon)
+
+            #  This line will generate warnings for null values but it is faster to
+            # process them afterwards using the nan_to_num
+            with np.errstate(invalid='ignore'):
+                M = np.nan_to_num(N + 1 - np.log(np.log(abs(Z)))/np.log(2) + log_horizon)
+            
+            M=light.shade(M,cmap=plt.cm.hot,vert_exag=1.5,norm=colors.PowerNorm(0.3),blend_mode='hsv')
+            plt.imshow(M, extent=[xmin, xmax, ymin, ymax], interpolation="bicubic")
+            ax.set_xticks([])
+            ax.set_yticks([])
+            
+            plt.savefig("frac"+str(i)+".png")
+            fraclist[i] = pygame.image.load("frac"+str(i)+".png").convert()
+            fraclist[i] = pygame.transform.scale(fraclist[i],(int(180*prop),int(150*prop)))
+            reclist[i] = fraclist[i].get_rect()
+            reclist[i] = reclist[i].move((int((180+50)*i*prop),int(20*prop)))
+            screen.blit(fraclist[i],reclist[i])
+            
+        
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                pygame.quit()
+            elif event.type == KEYDOWN:
+                    pygame.quit()
+        pygame.display.flip()
+    
+        fitness = [0,0,0,0]
+
+        print("Continuer ?")
+        q = input()
+        if q == 'no' or q == 'n':
+            quit = True
+
+        else:
+            print("Garder l'ancienne génération ou la nouvelle ? (nouvelle = 0, ancienne passer)")
+            i = int(input())
+            if i == 0:
+                for j in range(4):
+                    print("Préférences de l'image", j, "(entre 1 et 10) :")
+                    fitness[i] = int(input())
+                
+                print()
+                
+                #print(C)
+                C = crossover_all(C, fitness, 0.9)
+                #print(C)
+                C = mutation_all(C, 0.1, 0.1)
+
+                #print(C)
+
+    
+    
 
