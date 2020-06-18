660f12b1ddb00f9d24ea8aaa065ebc9a4251b9d8
nathan-faganello <faganello.nathan@gmail.com>
2020-06-18 18:38:45 +0200

    modification crossover1()

--- GA.py
+++ GA.py
@@ -1,4 +1,5 @@
 from random import Random
+from random import choices
 
 generator=Random()
 
@@ -19,33 +20,18 @@ def mutation(C,pm,sigma):
 	return C
 
 
-def crossover1(C_list,P,maxiter):
+def crossover1(C_list,P):
 	l=len(C_list[0])
 	C_cross=[]
 
-	p_adj=0                                       #on crée une liste P_final correspondant aux nombres d'éléments que l'on veut dans le C_cross 
+	p_adj=0                                       #on crée une liste P_final correspondant aux probabilités d'avoir un élément d'une liste C dans C_cross
 	for i in range(len(P)):
 		p_adj+=P[i]
-	P_final=[(maxiter/p_adj)*i for i in P]
+	P_final=[(1/p_adj)*i for i in P]
 
-	P_tot=0
-	for i in range(len(P_final)):
-		P_final[i]=int(P_final[i])
-		P_tot+=P_final[i]
-	erreur=maxiter-P_tot
 
-	rang_ajout=maximum(P_final)                    #si cela donne un nombre de termes < maxiter (longueur de la liste), on corrige sur l'élément le plus voulu 
-	P_final[rang_ajout]+=erreur
-	
-	if erreur != 0:
-		P_adj[0]+=erreur
-
-	for i in range(len(P_final)):                  # croisement
-		rang_choisi=maximum(P_final)
-		C=C_list[rang_choisi]
-		rang_max=P_final[rang_choisi]
-		for k in range(rang_max):
-			C_cross+=[C[k]]
-		P_final[rang_choisi]=-1
+	for j in range(l):                           #on choisi aléatoirement le coefficient dans une des listes C selon la préférence de l'utilisateur
+		C_choisi=choices(C_list,P_final)
+		C_cross+=[C_choisi[0][j]]
 
 	return C_cross
