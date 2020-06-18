171015d823d46785357883e21d990852ff1f16b5
nathan-faganello <faganello.nathan@gmail.com>
2020-06-18 19:03:58 +0200

    modif crossover1()

--- GA.py
+++ GA.py
@@ -20,18 +20,20 @@ def mutation(C,pm,sigma):
 	return C
 
 
-def crossover1(C_list,P):
-	l=len(C_list[0])
-	C_cross=[]
+def crossover1(C_list,P,pc):
+	x=generator.uniform(0,1)
+	if x<pc:
+		l=len(C_list[0])
+		C_cross=[]
 
-	p_adj=0                                       #on crée une liste P_final correspondant aux probabilités d'avoir un élément d'une liste C dans C_cross
-	for i in range(len(P)):
-		p_adj+=P[i]
-	P_final=[(1/p_adj)*i for i in P]
+		p_adj=0                                       #on crée une liste P_final correspondant aux probabilités d'avoir un élément d'une liste C dans C_cross
+		for i in range(len(P)):
+			p_adj+=P[i]
+		P_final=[(1/p_adj)*i for i in P]
 
 
-	for j in range(l):                           #on choisi aléatoirement le coefficient dans une des listes C selon la préférence de l'utilisateur
-		C_choisi=choices(C_list,P_final)
-		C_cross+=[C_choisi[0][j]]
+		for j in range(l):                           #on choisi aléatoirement le coefficient dans une des listes C selon la préférence de l'utilisateur
+			C_choisi=choices(C_list,P_final)
+			C_cross+=[C_choisi[0][j]]
 
 	return C_cross
